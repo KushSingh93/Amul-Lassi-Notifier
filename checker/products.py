8e1220268024cecf4c751422bf338fa952da49b4
KushOG93 <kushagara.singh@ongrid.in>
2025-12-17 12:06:27 +0530

    added milkshake

--- checker/products.py
+++ checker/products.py
@@ -9,7 +9,8 @@ PRODUCTS = {
         "name": "Amul High Protein Plain Lassi (Pack of 30)",
         "url": "https://shop.amul.com/en/product/amul-high-protein-plain-lassi-200-ml-or-pack-of-30"
     },
-    "milkshakke"
-        "name": "Amul High Protein Milkshakes (Pack of 30)",
+    "milkshake": {
+        "name": "Amul Kool Protein Milkshake (Pack of 30)",
         "url": "https://shop.amul.com/en/product/amul-kool-protein-milkshake-or-vanilla-180-ml-or-pack-of-30"
+    }
 }
